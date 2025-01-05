91aacaa16a30439e30c9da1f0698d4899248eb20
Ty Mason <hi@tygr.dev>
2025-01-05 16:00:56 +0000

    feat: update headers

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
 
 	return (
 		<main className="container">
-			<h1>Welcome to Tauri + React</h1>
+			<h1>Updater Example</h1>
 
 			<div className="row">
 				<a href="https://vitejs.dev" target="_blank">
