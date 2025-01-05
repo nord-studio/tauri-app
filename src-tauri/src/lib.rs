ae006fe0cf0e75fbb5cb1f618f6a03bfe830efb3
Ty Mason <hi@tygr.dev>
2025-01-05 15:51:57 +0000

    feat: add updater button

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,3 @@
-use tauri_plugin_updater::UpdaterExt;
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,40 +8,10 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                update(handle).await.unwrap();
-            });
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app.updater()?.check().await? {
-        let mut downloaded = 0;
-
-        // alternatively we could also call update.download() and update.install() separately
-        update
-            .download_and_install(
-                |chunk_length, content_length| {
-                    downloaded += chunk_length;
-                    println!("downloaded {downloaded} from {content_length:?}");
-                },
-                || {
-                    println!("download finished");
-                },
-            )
-            .await?;
-
-        println!("update installed");
-        app.restart();
-    }
-
-    Ok(())
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,102 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { check } from "@tauri-apps/plugin-updater";
+import { ask, message } from "@tauri-apps/plugin-dialog";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+	const [greetMsg, setGreetMsg] = useState("");
+	const [name, setName] = useState("");
+	const [loading, setLoading] = useState(false);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		setGreetMsg(await invoke("greet", { name }));
+	}
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+	async function checkForUpdates() {
+		setLoading(true);
+		const update = await check();
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+		if (update === null) {
+			await message("You're on the latest version!", {
+				title: "Up to date",
+				kind: "info",
+				okLabel: "OK",
+			});
+		} else if (update.available) {
+			const yes = await ask(
+				"An update is available. Do you want to install it?",
+				{
+					title: "Update Available",
+					kind: "info",
+					okLabel: "Update",
+					cancelLabel: "Cancel",
+				}
+			);
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+			if (yes) {
+				await update.downloadAndInstall();
+				await invoke("graceful_restart");
+			}
+		}
+
+		setLoading(false);
+	}
+
+	return (
+		<main className="container">
+			<h1>Welcome to Tauri + React</h1>
+
+			<div className="row">
+				<a href="https://vitejs.dev" target="_blank">
+					<img
+						src="/vite.svg"
+						className="logo vite"
+						alt="Vite logo"
+					/>
+				</a>
+				<a href="https://tauri.app" target="_blank">
+					<img
+						src="/tauri.svg"
+						className="logo tauri"
+						alt="Tauri logo"
+					/>
+				</a>
+				<a href="https://reactjs.org" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
+			</div>
+			<p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+			<div>
+				<button onClick={checkForUpdates} id="update-button">
+					{loading ? "Loading..." : "Check for updates"}
+				</button>
+			</div>
+
+			<form
+				className="row"
+				onSubmit={(e) => {
+					e.preventDefault();
+					greet();
+				}}
+			>
+				<input
+					id="greet-input"
+					onChange={(e) => setName(e.currentTarget.value)}
+					placeholder="Enter a name..."
+				/>
+				<button type="submit">Greet</button>
+			</form>
+			<p>{greetMsg}</p>
+		</main>
+	);
 }
 
 export default App;
