9d624a8a9c092f583e4bc748d84960cd9b35bf2d
Ty Mason <hi@tygr.dev>
2025-01-05 16:26:18 +0000

    feat: add version number

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { check } from "@tauri-apps/plugin-updater";
 import { ask, message } from "@tauri-apps/plugin-dialog";
+import { app } from "@tauri-apps/api";
 import "./App.css";
 
 function App() {
 	const [greetMsg, setGreetMsg] = useState("");
 	const [name, setName] = useState("");
 	const [loading, setLoading] = useState(false);
+	const [version, setVersion] = useState("");
 
 	async function greet() {
 		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 		setGreetMsg(await invoke("greet", { name }));
+	}
+
+	async function getVersion() {
+		setVersion(await app.getVersion());
 	}
 
 	async function checkForUpdates() {
@@ -38,41 +43,21 @@ function App() {
 
 			if (yes) {
 				await update.downloadAndInstall();
-				await invoke("graceful_restart");
+				await app;
 			}
 		}
 
 		setLoading(false);
 	}
 
+	useEffect(() => {
+		getVersion();
+	}, []);
+
 	return (
 		<main className="container">
 			<h1>Updater Example</h1>
-
-			<div className="row">
-				<a href="https://vitejs.dev" target="_blank">
-					<img
-						src="/vite.svg"
-						className="logo vite"
-						alt="Vite logo"
-					/>
-				</a>
-				<a href="https://tauri.app" target="_blank">
-					<img
-						src="/tauri.svg"
-						className="logo tauri"
-						alt="Tauri logo"
-					/>
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<p>Click on the Tauri, Vite, and React logos to learn more.</p>
+			<p>Running: {version}</p>
 
 			<div>
 				<button onClick={checkForUpdates} id="update-button">
